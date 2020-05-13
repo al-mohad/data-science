4ca2c1582be3daab6eab2415299b3947d75ce27a
Muhammad Buhari <gmbdairy@gmail.com>
2020-05-13 06:53:11 +0100

    first commit

---
+++ ReallySmartBrain/brain.py
@@ -0,0 +1,17 @@
+# pip3 install -U tensorflow keras opencv-python
+# pip3 install imageai --upgrade
+
+from imageai.Prediction import ImagePrediction
+import os
+execution_path = os.getcwd()
+
+prediction = ImagePrediction()
+prediction.setModelTypeAsSqueezeNet()
+prediction.setModelPath(os.path.join(
+    execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.predictImage(
+    os.path.join(execution_path, "giraffe.jpg"), result_count=5)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction, " : ", eachProbability)
